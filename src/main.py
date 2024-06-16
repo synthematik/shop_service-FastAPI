99e770d19af864c72433aba48d43e8cf436f245b
Garik <garfild_2003@mail.ru>
2024-06-16 16:12:50 +0300

    Added CORS

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from sqladmin import Admin
 
 from src.admin.auth import authentication_backend
@@ -30,4 +31,21 @@ admin.add_view(ProductsImagesAdmin)
 
 admin.add_view(OrdersAdmin)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8080",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#app.mount("/static", StaticFiles(directory="static"), name="static")
